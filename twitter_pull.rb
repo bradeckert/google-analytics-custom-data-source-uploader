412973f4a77fcd2d067894e8bc3503fe8a079b28
Brad Eckert <beckert@mit.edu>
2014-01-23 16:52:42 -0800

    Twitter close

--- twitter_pull.rb
+++ twitter_pull.rb
@@ -1,41 +1,39 @@
 require 'selenium-webdriver'
+require 'headless'
 require './twitter_csv'
 
 def get_twitter_data
-	headless = Headless.new(reuse: true, destroy_at_exit:false)
-	headless.start
-
+#	headless = Headless.new(reuse: true, destroy_at_exit:false)
+#	headless.start
 
 	profile = Selenium::WebDriver::Firefox::Profile.new
-	profile['browser.download.dir'] = "~/nest/adcosttracking"
+	profile['browser.download.dir'] = "~/adcosttracker"
 	profile['browser.download.folderList'] = 2
 	profile['browser.helperApps.neverAsk.saveToDisk'] = 'text/csv'
 	profile['pdfjs.disabled'] = true
 
 	driver = Selenium::WebDriver.for :firefox, :profile => profile
-	driver.manage.window.resize_to(800,800)
 
 	# Load personal data (login/password)
   	# from persoal_data.json.
   	personal_data = JSON.parse(File.read('personal_data.json'))["download"]["twitter"]
 
-	driver.navigate.to "https://ads.twitter.com/accounts/2s6hi3/campaigns"
-	username = driver.find_elements(:name, 'user_identifier')[1]
-	password = driver.find_elements(:name, 'password')[1]
+	driver.navigate.to "https://twitter.com/login?redirect_after_login=https%3A%2F%2Fads.twitter.com%2Faccounts%#{personal_data["accountId"]}%2Fcampaigns_dashboard"
+	username = driver.find_elements(:name, 'session[username_or_email]')[1]
+	password = driver.find_elements(:name, 'session[password]')[1]
 	username.send_keys personal_data["login"]
 	password.send_keys personal_data["password"]
 	password.submit
 
-	campaigns = driver.find_elements(:id, "sortableRow-2")
-	campaigns.each do |campaign|
-		status = campaign.find_element(:class, "status-label")
-		if status.text == "ACTIVE"
-			campaign.find_element(:class, "download-csv-link").click
-			title = campaign.find_element(:class, "campaign-dashboard-link").text
-			convert_and_upload("Daily spend report for_ " + title + ".csv")
-		end
-	end
+        wait = Selenium::WebDriver::Wait.new(:timeout => 15) # seconds
+	
+	wait.until { driver.find_element(:class, 'popup-close') }
+	driver.find_element(:class, 'popup-close').click
+	wait.until { driver.find_element(:class, 'csvButtonContainer') }
+	driver.find_element(:class, 'csvButtonContainer').click
 
+	sleep(8)
+	convert_and_upload("metrics.csv")
 	driver.quit
 end
 
